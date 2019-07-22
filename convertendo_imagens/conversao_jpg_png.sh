b82093524f55944d8483b59e427a1145f5cd90a1
Arthur Siqueira <arthur.siq5@gmail.com>
2019-07-22 20:04:32 -0300

    usado laço de repetição para executar parâmetros

--- convertendo_imagens/conversao_jpg_png.sh
+++ convertendo_imagens/conversao_jpg_png.sh
@@ -2,7 +2,9 @@
 
 CAMINHO_IMAGENS=~/projetos/shell_script_alura/convertendo_imagens/imagens-livros
 
-convert $CAMINHO_IMAGENS/$1.jpg $CAMINHO_IMAGENS/$1.png
-
-convert $CAMINHO_IMAGENS/$2.jpg $CAMINHO_IMAGENS/$2.png
-
+for imagem in $@
+do
+	convert $CAMINHO_IMAGENS/$imagem.jpg $CAMINHO_IMAGENS/$imagem.png
+done
+echo -e "\n\n---\\\\\\---"
+echo -e "\n\nas imagens selecionadas foram convertidas"
