d93846a7a150c0fe633334dce404e4dbfed18973
Arthur Siqueira <arthur.siq5@gmail.com>
2019-07-22 21:58:20 -0300

    modificado código para usar uma função para realizar as conversões

--- convertendo_imagens/conversao_jpg_png.sh
+++ convertendo_imagens/conversao_jpg_png.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-
-# se o diretório png não existir, crie-o
-if [ ! -d png ]
-then
-	mkdir png
-fi
-
-
-cd ./imagens-livros
+converte_imagem(){
+	# se o diretório png não existir, crie-o
+	if [ ! -d png ]
+	then
+		mkdir png
+	fi
 
 
-for imagem in *.jpg # percorre um vetor com todos os arquivos com extensão '.jpg'
-do
-	# pegando o nome da imagem sem extensão
-	imagem_sem_extensao=$(ls $imagem | awk -F . '{ print $1 }')
-	convert $imagem ../png/$imagem_sem_extensao.png
-done
-echo -e "\n\n---\\\\\\---"
-echo -e "\n\nas imagens selecionadas foram convertidas"
+	cd ./imagens-livros
+
+
+	for imagem in *.jpg # percorre um vetor com todos os arquivos com extensão '.jpg'
+	do
+		# pegando o nome da imagem sem extensão
+		local imagem_sem_extensao=$(ls $imagem | awk -F . '{ print $1 }')
+		convert $imagem ../png/$imagem_sem_extensao.png
+	done
+}
+
+converte_imagem
+echo $imagem_sem_extensao
+#echo -e "\n\n---\\\\\\---"
+#echo -e "\n\nas imagens selecionadas foram convertidas"
